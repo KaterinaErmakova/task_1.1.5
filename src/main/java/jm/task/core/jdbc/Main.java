dd59bbd77369da045c4bb81bc13903838e116e0e
Katerina_Ermakova <katrin6793@gmail.com>
2025-02-03 19:57:45 +0300

    fixes

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,18 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.dao.UserDao;
+import jm.task.core.jdbc.dao.UserDaoHibernateImpl;
+
 public class Main {
     public static void main(String[] args) {
-
+        UserDao userDao = new UserDaoHibernateImpl();
+        userDao.createUsersTable();
+        userDao.saveUser("Vasiliy", "Livanov", (byte) 89);
+        userDao.saveUser("Alla", "Pugacheva", (byte) 76);
+        userDao.saveUser("Jason", "Statham", (byte) 57);
+        userDao.saveUser("Johnny", "Sins", (byte) 46);
+        System.out.println(userDao.getAllUsers());
+        userDao.cleanUsersTable();
+        userDao.dropUsersTable();
     }
 }
